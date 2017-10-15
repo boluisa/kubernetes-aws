aae625e325f7060ad4ce27ba8a24f836202aabf3
Vishravars Ramasubramanian <vishravars.ramasubramanian@philips.com>
2017-10-15 11:46:40 +0530

    Advanced configurations added

--- aws-terra-starter/variables.tf
+++ aws-terra-starter/variables.tf
@@ -8,3 +8,16 @@ variable "dbpassword" {}
 variable "key_name" {}
 variable "public_key_path" {}
 variable "domainname" {}
+variable "dev_instance_type" {}
+variable "dev_instance_ami" {}
+variable "elb_healthy_threshold" {}
+variable "elb_unhealthy_threshold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
+variable "asg_max" {}
+variable "asg_min" {}
+variable "asg_grace" {}
+variable "asg_hct" {}
+variable "asg_cap" {}
+variable "lc_instance_type" {}
+variable "delegation_set" {}
